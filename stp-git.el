53447a3116110afdda195338ca8bf0c65b1e4ae9
David J. Rosenbaum <djr7c4@gmail.com>
2025-03-10 13:16:08 -0700

    Add license and license headers

--- stp-git.el
+++ stp-git.el
@@ -1,4 +1,18 @@
 ;;; -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2025, David J. Rosenbaum <djr7c4@gmail.com>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of version 3 of the GNU General Public License, as
+;; published by the Free Software Foundation.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (require 'cl-lib)
 (require 's)
