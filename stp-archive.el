772ce549026481ebefa34f3e4676281d60d77f5f
David J. Rosenbaum <djr7c4@gmail.com>
2025-04-08 12:07:59 -0700

    Don't refresh package archives unless it is more than the interval

--- stp-archive.el
+++ stp-archive.el
@@ -16,31 +16,55 @@
 
 (require 'async)
 (require 'package)
+(require 'persist nil t)
 
 (defvar stp-archive-async-refresh-running nil)
 
-(cl-defun stp-archive-async-refresh (&key quiet)
-  "Refresh the package archive asynchronously in a separate process."
-  (interactive)
-  (if stp-archive-async-refresh-running
-      (user-error "`stp-archive-async-refresh' is already running")
-    (setq stp-archive-async-refresh-running t)
+(with-no-warnings
+  (when (featurep 'persist)
+    (persist-defvar stp-archive-last-refreshed most-negative-fixnum nil)))
+
+(defvar stp-archive-last-refreshed most-negative-fixnum)
+
+(defvar stp-archive-refresh-interval (timer-duration "1 day")
+  "The number of seconds until the package archives are considered
+stale and should be refreshed.")
+
+(cl-defun stp-archive-async-refresh (&key force quiet)
+  "Refresh the package archive asynchronously in a separate process.
+When FORCE is non-nil or with a prefix argument interactively,
+refresh even if the last refresh was less than
+`stp-archive-refresh-interval' seconds ago."
+  (interactive (list :force current-prefix-arg))
+  (when stp-archive-async-refresh-running
+    (user-error "`stp-archive-async-refresh' is already running"))
+  ;; Only refresh when it has been at least `stp-archive-refresh-interval'
+  ;; seconds since the last refresh.
+  (if (or force
+          (> (- (rem-seconds) stp-archive-last-refreshed)
+             stp-archive-refresh-interval))
+      (progn
+        (setq stp-archive-async-refresh-running t
+              stp-archive-last-refreshed (rem-seconds))
+        (unless quiet
+          (message "Refreshing package archives asynchronously"))
+        (async-start
+         `(lambda ()
+            (require 'package)
+            ,(async-inject-variables "^package-archives$")
+            (package-refresh-contents)
+            nil)
+         (lambda (_result)
+           ;; Receiving `package-archive-contents' from the child process is very slow
+           ;; because it is so large. It is much faster to just read the package
+           ;; archive from disk instead.
+           (package-read-all-archive-contents)
+           (setq stp-archive-async-refresh-running nil)
+           (unless quiet
+             (message "Asynchronous refresh of the package archives finished")))))
     (unless quiet
-      (message "Refreshing package archives asynchronously"))
-    (async-start
-     `(lambda ()
-        (require 'package)
-        ,(async-inject-variables "^package-archives$")
-        (package-refresh-contents)
-        nil)
-     (lambda (_result)
-       ;; Receiving `package-archive-contents' from the child process is very slow
-       ;; because it is so large. It is much faster to just read the package
-       ;; archive from disk instead.
-       (package-read-all-archive-contents)
-       (setq stp-archive-async-refresh-running nil)
-       (unless quiet
-         (message "Asynchronous refresh of the package archives finished"))))))
+      (message "The package archives were last refreshed on %s: no refresh is necessary"
+               (format-time-string "%c" stp-archive-last-refreshed)))))
 
 (defun stp-package-requirements (pkg-name)
   "Find all packages that are required by PKG-NAME according to the
