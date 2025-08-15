cf8746e6e470c8794af7238933d283393a437f44
David J. Rosenbaum <djr7c4@gmail.com>
2025-08-15 11:06:58 -0700

    Update package requirements

--- stp.el
+++ stp.el
@@ -5,7 +5,7 @@
 ;; Keywords: git tools
 ;; URL: https://github.com/djr7C4/subtree-package
 ;; Version: 0.8.4
-;; Package-Requires: ((emacs "29.1") (dash "2.19.1") (f "0.21.0") (s "1.12.0") (queue "0.2") (async "1.9.9") (anaphora "1.0.4") (memoize "1.2.0") (rem "0.6.0"))
+;; Package-Requires: ((emacs "29.1") (dash "2.19.1") (f "0.21.0") (s "1.12.0") (queue "0.2") (async "1.9.9") (anaphora "1.0.4") (memoize "1.2.0") (rem "0.7.6"))
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of version 3 of the GNU General Public License, as
