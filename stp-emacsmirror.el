fbcd394dc4789b7b11a04bd9633b9502d65ce26f
David J. Rosenbaum <djr7c4@gmail.com>
2025-09-16 12:13:56 -0700

    Use read-symbol-shorthands instead of rem-abbrev

--- stp-emacsmirror.el
+++ stp-emacsmirror.el
@@ -120,4 +120,38 @@ is an alist that maps remotes to methods."
       (match-string 1 remote))))
 
 (provide 'stp-emacsmirror)
+
+;; Local Variables:
+;; read-symbol-shorthands: (
+;;   ("db" . "cl-destructuring-bind")
+;;   ("mvb" . "cl-multiple-value-bind")
+;;   ("mvs" . "cl-multiple-value-setq")
+;;   ("with-gensyms" . "cl-with-gensyms")
+;;   ("once-only" . "cl-once-only")
+;;   ("dflet" . "noflet")
+;;   ("plet" . "pcase-let")
+;;   ("plet*" . "pcase-let*")
+;;   ("psetq*" . "pcase-setq")
+;;   ("pdolist" . "pcase-dolist")
+;;   ("plambda" . "pcase-lambda")
+;;   ("pdefmacro" . "pcase-defmacro")
+;;   ("epcase" . "pcase-exhaustive")
+;;   ("fn" . "rem-fn")
+;;   ("fn1" . "rem-fn1")
+;;   ("fn2" . "rem-fn2")
+;;   ("fn3" . "rem-fn3")
+;;   ("fn4" . "rem-fn4")
+;;   ("fn5" . "rem-fn5")
+;;   ("fn6" . "rem-fn6")
+;;   ("fn7" . "rem-fn7")
+;;   ("fn8" . "rem-fn8")
+;;   ("fn9" . "rem-fn9")
+;;   ("fn10" . "rem-fn10")
+;;   ("and$" . "cond-let--and$")
+;;   ("and>" . "cond-let--and>")
+;;   ("and-let" . "cond-let--and-let")
+;;   ("if-let" . "cond-let--if-let")
+;;   ("when-let" . "cond-let--when-let")
+;;   ("while-let" . "cond-let--while-let"))
+;; End:
 ;;; stp-emacsmirror.el ends here
