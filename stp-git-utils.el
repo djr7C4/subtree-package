2d5a514b2a66329826d5931eece2ba1c58af8e06
David J. Rosenbaum <djr7c4@gmail.com>
2026-04-09 14:25:58 -0700

    Remove extraneous header information

--- stp-git-utils.el
+++ stp-git-utils.el
@@ -1,24 +1,4 @@
 ;;; stp-git-utils.el --- Git utility functions -*- lexical-binding: t; -*-
-;;
-;; Copyright (C) 2025 David J. Rosenbaum <djr7c4@gmail.com>
-
-;; Author: David J. Rosenbaum <djr7c4@gmail.com>
-;; Keywords: TODO
-;; URL: https://github.com/djr7C4/subtree-package
-;; Version: 0.11.4
-;; Package-Requires: nil
-;;
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of version 3 of the GNU General Public License, as
-;; published by the Free Software Foundation.
-;;
-;; This program is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Code:
 
