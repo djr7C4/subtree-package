185abe120dc2961937dd7ef1d6bff09fc814b171
David J. Rosenbaum <djr7c4@gmail.com>
2026-09-09 16:30:55 -0700

    Add license header to stp.el

--- stp.el
+++ stp.el
@@ -1,5 +1,19 @@
 ;;; stp.el --- Alias for subtree-package.el -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2026 David J. Rosenbaum <djr7c4@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of version 3 of the GNU General Public License, as
+;; published by the Free Software Foundation.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Code:
 
 (require 'subtree-package)
